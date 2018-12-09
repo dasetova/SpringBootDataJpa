69f2529b35568ef39ec25a4fb96f452eafcc0d32
Daniel Torres <dasetova@gmail.com>
2018-12-09 12:47:46 -0500

    Changing authorization with JDBC

--- src/main/java/com/dasetova/springstudy/SpringBootDataJpaApplication.java
+++ src/main/java/com/dasetova/springstudy/SpringBootDataJpaApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.dasetova.springstudy.models.service.IUploadFileService;
 
@@ -12,6 +13,9 @@ public class SpringBootDataJpaApplication implements CommandLineRunner{
 
 	@Autowired
 	private IUploadFileService uploadFileService;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
@@ -22,5 +26,11 @@ public class SpringBootDataJpaApplication implements CommandLineRunner{
 		// TODO Auto-generated method stub
 		uploadFileService.deleteAll();
 		uploadFileService.init();
+		
+		String passowrd = "12345";
+		for(int i = 0; i<2; i++) {
+			String bcryptPassword = passwordEncoder.encode(passowrd);
+			System.out.println("Passowrd encoded: " + bcryptPassword);
+		}
 	}
 }
